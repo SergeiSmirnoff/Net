4c7c3b81b186600b5b011130d46a2663af087661
HOBOT <sergeiismirnoff@gmail.com>
2018-05-19 15:05:55 +0300

    Initial Commit

---
+++ Networking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Сергей on 19.05.2018.
+//  Copyright © 2018 Сергей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
